d4f3245dcf7f51ec2871b79b648c88a079d727e1
Daniel Oloruntoba <danieloloruntoba681@gmail.com>
2023-05-28 21:30:59 +0100

    edited documents

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const decrement = () => {
+    setCount((count) => count - 1);
+  };
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + FAVOUR</h1>
+      <div className="card">
+        <button
+          onClick={() => setCount((count) => count + 1)}
+          style={{ marginRight: "1.5rem" }}
+        >
+          count is {count}
+        </button>
+        <button onClick={decrement} disabled={count <= 0 ? true : false}>
+          Decrement
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
