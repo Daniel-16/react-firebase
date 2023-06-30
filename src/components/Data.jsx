78ae5d34f84ac54b0b08c8b730cd899b6e007c28
Daniel Oloruntoba <danieloloruntoba681@gmail.com>
2023-06-30 21:01:33 +0100

    Created error boundaries

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -5,6 +5,7 @@ import { getDocs, collection } from "firebase/firestore";
 export default function Data() {
   const [users, setUsers] = useState([]);
   const [loading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
   const userCollectionRef = collection(db, "users");
   useEffect(() => {
     const getUser = async () => {
@@ -20,28 +21,55 @@ export default function Data() {
       } catch (error) {
         console.error(error);
         setIsLoading(false);
+        setError("An error occured while loading data");
       }
     };
     getUser();
   }, []);
 
-  return (
-    <div className="container">
-      <h1 className="text-center">Admin Dashboard</h1>
-      {loading ? (
-        <div className="text-center">
-          <div className="spinner-grow"></div>
-        </div>
-      ) : (
-        users.map((user) => (
+  // return (
+  //   <div className="container">
+  //     <h1 className="text-center">Admin Dashboard</h1>
+  //     {loading ? (
+  //       <div className="text-center">
+  //         <div className="spinner-grow"></div>
+  //       </div>
+  //     ) : (
+  //       users.map((user) => (
+  //         <div className="card mb-2" key={user.id}>
+  //           <div className="card-body">
+  //             <h3 className="card-title">{user.name}</h3>
+  //             <p className="card-text">{user.age}</p>
+  //           </div>
+  //         </div>
+  //       ))
+  //     )}
+  //   </div>
+  // );
+  if (loading) {
+    return (
+      <div className="container mt-4 text-center">
+        <div className="spinner-grow"></div>
+      </div>
+    );
+  } else if (error !== null) {
+    return (
+      <div className="container pt-3">
+        <h3 className="text-center">An error occured while loading.</h3>
+      </div>
+    );
+  } else {
+    return (
+      <div className="container pt-3">
+        {users.map((user) => (
           <div className="card mb-2" key={user.id}>
             <div className="card-body">
               <h3 className="card-title">{user.name}</h3>
               <p className="card-text">{user.age}</p>
             </div>
           </div>
-        ))
-      )}
-    </div>
-  );
+        ))}
+      </div>
+    );
+  }
 }
